f601cd0fed62debfb4a0de9fabe667cdcd12ddff
Jared Shimabukuro <jshima@hawaii.edu>
2017-02-07 14:36:04 -1000

    Completed bank.cpp

--- Labs/Lab4/bank.cpp
+++ Labs/Lab4/bank.cpp
@@ -7,7 +7,7 @@
  * Lab Section: 001
  *
  * Date Started: 2/7/17
- * Date Finished: 
+ * Date Finished: 2/7/17 
  */
 
 #include "savingsAccount.h"
@@ -48,10 +48,137 @@ int main(){
 
 	num = rand();
 
-	xing.setAccountNumber(num);
-	xing.setName("Xing");
-	xing.setBalance(5000);
-	xing.setWithdrawNumber(0);
+	for(int savingsCount = 0; savingsCount < 3; savingsCount++){
+		if(savingsCount == 0){
+			xing.setAccountNumber(num);
+			xing.setName("Xing");
+			xing.setBalance(5000);
+			xing.setWithdrawNumber(0);
+		}
+
+		else if(savingsCount == 1){
+			yi.setAccountNumber(num);
+			yi.setName("Yi");
+			yi.setBalance(4000);
+			yi.setWithdrawNumber(1);
+		}
+
+		else if(savingsCount == 2){
+			zed.setAccountNumber(num);
+			zed.setName("Zed");
+			zed.setBalance(6000);
+			zed.setWithdrawNumber(2);
+		}
+
+		num++;
+	}
+
 
 	xing.prettyprint();
+	yi.prettyprint();
+	zed.prettyprint();
+
+	while(1){
+		char command;
+		char n;
+		cout << "Commands: (W)ithdraw, (D)eposit, (P)rint, (Q)uit" << endl << "Please select a command:";
+		cin >> command;
+		if(command == 'w' || command == 'W'){
+			cout << "Accounts: (A)lice, (B)ob, (C)harlie, (X)ing, (Y)i, (Z)ed" << endl << "Please select an account: ";
+			cin >> n;
+			
+			float amount;
+			cout << "Please enter the amount of money to withdraw: ";
+			cin >> amount;
+		
+			switch(n){
+				case 'a':
+				case 'A': alice.withdraw(amount);
+					  break;
+				case 'b':
+				case 'B': bob.withdraw(amount);
+					  break;
+				case 'c':
+				case 'C': charlie.withdraw(amount);
+					  break;
+				case 'x':
+				case 'X': xing.withdraw(amount);
+					  xing.monthly_check();
+					  break;
+				case 'y':
+				case 'Y': yi.withdraw(amount);
+					  yi.monthly_check();
+					  break;
+				case 'z':
+				case 'Z': zed.withdraw(amount);
+					  zed.monthly_check();
+					  break;
+				default: cout << "Invalid Account" << endl;
+			}
+			cout << "Withdrew " << amount << " from account." << endl;
+			cout << endl;
+		}
+
+               else if(command == 'd' || command == 'D'){
+                        cout << "Accounts: (A)lice, (B)ob, (C)harlie, (X)ing, (Y)i, (Z)ed" << endl << "Please select an account: ";
+                        cin >> n;
+
+                        float amount;
+                        cout << "Please enter the amount of money to deposit: ";
+                        cin >> amount;
+
+                        switch(n){
+                                case 'a':
+                                case 'A': alice.deposit(amount);
+                                          break;
+                                case 'b':
+                                case 'B': bob.deposit(amount);
+                                          break;
+                                case 'c':
+                                case 'C': charlie.deposit(amount);
+                                          break;
+                                case 'x':
+                                case 'X': xing.deposit(amount);
+                                          break;
+                                case 'y':
+                                case 'Y': yi.deposit(amount);
+                                          break;
+                                case 'z':
+                                case 'Z': zed.deposit(amount);
+                                          break;
+                                default: cout << "Invalid Account" << endl;
+                        }
+			cout << "Deposited " << amount << " into account." << endl;
+			cout << endl;
+                }
+
+		else if(command == 'p' || command == 'P'){
+                        cout << "Accounts: (A)lice, (B)ob, (C)harlie, (X)ing, (Y)i, (Z)ed" << endl << "Please select an account: ";
+                        cin >> n;
+
+                        switch(n){
+                                case 'a':
+                                case 'A': alice.prettyprint();
+                                          break;
+                                case 'b':
+                                case 'B': bob.prettyprint();
+                                          break;
+                                case 'c':
+                                case 'C': charlie.prettyprint();
+                                          break;
+                                case 'x':
+                                case 'X': xing.prettyprint();
+                                          break;
+                                case 'y':
+                                case 'Y': yi.prettyprint();
+                                          break;
+                                case 'z':
+                                case 'Z': zed.prettyprint();
+                                          break;
+                                default: cout << "Invalid Account" << endl;
+                        }
+                }
+
+		else if(command == 'q' || command == 'Q') return 0;
+	}
 }
